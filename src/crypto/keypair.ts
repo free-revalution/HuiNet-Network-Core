4ac1b26f275217637259ddd88a9ad001216cf220
jiang <2035645950@qq.com>
2026-03-16 14:58:52 +0800

    fix: add missing deriveNodeID function to crypto module

--- src/crypto/keypair.ts
+++ src/crypto/keypair.ts
@@ -1,4 +1,6 @@
 import * as nacl from 'tweetnacl';
+import { createHash } from 'crypto';
+import { Base58 } from '../utils/base58';
 
 export interface KeyPair {
   publicKey: Buffer;
@@ -24,4 +26,13 @@ export function generateKeyPair(): KeyPair {
 export function getPublicKey(privateKey: Buffer): Buffer {
   // In Ed25519, the public key is the last 32 bytes of the secret key
   return privateKey.subarray(32);
+}
+
+/**
+ * Derive NodeID from public key
+ * NodeID = Base58(SHA256(publicKey))
+ */
+export function deriveNodeID(publicKey: Buffer): string {
+  const hash = createHash('sha256').update(publicKey).digest();
+  return Base58.encode(hash);
 }
